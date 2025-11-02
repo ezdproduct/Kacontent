f1f8c3abeef95beeaad2a694ebede7308736c8a6
Ryan Huynh <ryanhuynh1803@gmail.com>
2025-11-02 15:45:48 +0700

    [dyad] Tối ưu hóa bố cục cho máy tính bảng - wrote 2 file(s)

--- src/components/latest-posts.tsx
+++ src/components/latest-posts.tsx
@@ -48,7 +48,7 @@ export const LatestPosts = () => {
           </p>
         </div>
 
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
           {posts.map((post, index) => (
             <BlogPostCard key={index} post={post} />
           ))}
